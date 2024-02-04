15bd1082e6862b9b2410788b1fb758cbacb6e7e7
branmarvel <88461465+branmarvel@users.noreply.github.com>
2024-02-04 18:33:36 -0400

    arreglado cositas

--- src/app/containers/default-layout/_nav.ts
+++ src/app/containers/default-layout/_nav.ts
@@ -241,22 +241,22 @@ export const navItems: INavData[] = [
       }
     ]
   },
-  {
-    title: true,
-    name: 'Links',
-    class: 'py-0'
-  },
-  {
-    name: 'Docs',
-    url: 'https://coreui.io/angular/docs/templates/installation',
-    iconComponent: { name: 'cil-description' },
-    attributes: { target: '_blank', class: '-text-dark' },
-    class: 'mt-auto'
-  },
-  {
-    name: 'Try CoreUI PRO',
-    url: 'https://coreui.io/product/angular-dashboard-template/',
-    iconComponent: { name: 'cil-layers' },
-    attributes: { target: '_blank' }
-  }
+  // {
+  //   title: true,
+  //   name: 'Links',
+  //   class: 'py-0'
+  // },
+  // {
+  //   name: 'Docs',
+  //   url: 'https://coreui.io/angular/docs/templates/installation',
+  //   iconComponent: { name: 'cil-description' },
+  //   attributes: { target: '_blank', class: '-text-dark' },
+  //   class: 'mt-auto'
+  // },
+  // {
+  //   name: 'Try CoreUI PRO',
+  //   url: 'https://coreui.io/product/angular-dashboard-template/',
+  //   iconComponent: { name: 'cil-layers' },
+  //   attributes: { target: '_blank' }
+  // }
 ];
